9d568dcba1d40e037d50185fc424a75a82e2e35b
Roland <rbayor16@gmail.com>
2022-09-30 07:42:11 +0000

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col text-center justify-center h-screen text-slate-700 text-5xl font-semibold">
+      Songtama Pig Farm
+    </div>
+  );
+};
+
+export default Home;
