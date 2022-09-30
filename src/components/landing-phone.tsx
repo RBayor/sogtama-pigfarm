57f2a59ff8f43c199c01dc967b2b469c618d9ddc
Roland <rbayor16@gmail.com>
2022-09-30 11:23:48 +0000

    desktop layout

---
+++ src/components/landing-phone.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const LandingPhone = () => {
+  return (
+    <div className="lg:hidden">
+      <div className="h-screen w-full"></div>
+
+      <div className="flex flex-col w-full items-center p-3 absolute mt-10 right-0 top-0 mx-auto text-center space-y-3 text-3xl text-slate-50 reletive font-bold uppercase">
+        <Image
+          className="relative"
+          src="/img/logo.png"
+          layout="fixed"
+          height={150}
+          width={200}
+          priority
+        />
+        <h1 className="">Songtama</h1>
+        <h2 className="">Pig Farm</h2>
+        <p className="text-xl">high quality organically feed pigs</p>
+        <Link href="/gallery">
+          <a className="text-xl max-w-xs ring-2 p-4 ring-emerald-500 hover:bg-emerald-700 rounded">
+            Gallery
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+// <div className="flex flex-col w-full items-center p-3 absolute mt-10 right-0 top-0 mx-auto text-center space-y-3 text-3xl text-slate-50 reletive font-bold uppercase">
+//   <Image
+//     className="relative"
+//     src="/img/logo.png"
+//     layout="fixed"
+//     height={150}
+//     width={200}
+//     priority
+//   />
+//   <h1 className="shadow-lg">Songtama</h1>
+//   <h2 className="shadow-lg">Pig Farm</h2>
+//   <p className="text-2xl">high quality organically feed pigs</p>
+//   <Link href="/gallery">
+//     <a className="text-xl max-w-xs ring-2 p-4 ring-emerald-500 hover:bg-emerald-700 rounded">
+//       Gallery
+//     </a>
+//   </Link>
+// </div>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,126 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { ImageWrapper } from "../components/imgWrapper";
+import { Info } from "../components/info";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col text-center justify-center h-screen text-slate-700 text-5xl font-semibold">
-      Songtama Pig Farm
-    </div>
+    <>
+      <Head>
+        <title>Songtama - Pig Farm</title>
+        <meta
+          name="description"
+          content="Saving and financial maanagement solution"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex flex-col min-h-screen w-full">
+        <div>
+          <Image
+            className="relative blur-sm"
+            src="/pig.png"
+            layout="responsive"
+            height={512}
+            width={1440}
+            priority
+          />
+          <div className="absolute mt-10 right-0 top-0  text-center space-y-3 mr-28 text-7xl text-slate-50 reletive font-bold uppercase">
+            <Image
+              className="relative"
+              src="/img/logo.png"
+              layout="fixed"
+              height={150}
+              width={200}
+              priority
+            />
+            <h1 className="shadow-lg">Songtama</h1>
+            <h2 className="shadow-lg">Pig Farm</h2>
+            <p className="text-2xl">high quality organically feed pigs</p>
+            <Link href="/gallery">
+              <a className="text-xl ring-2 p-4 ring-emerald-500 hover:bg-emerald-700 rounded">
+                Gallery
+              </a>
+            </Link>
+          </div>
+        </div>
+        <main className="flex flex-col w-full items-center">
+          <div className="container">
+            <section className="flex w-full justify-center">
+              <div className="justify-evenly w-full flex flex-row text-emerald-600 text-3xl mt-20">
+                <Info stat={150} title={"population"} /> |
+                <Info stat={12} title={"happy customers"} /> |
+                <Info stat={5} title={"years running"} />
+              </div>
+            </section>
+
+            <section className="flex flex-wrap text-center mt-20">
+              <div className="w-full xl:w-7/12">
+                <ImageWrapper
+                  src={"/svg/contact.svg"}
+                  alt={"savings with a piggy bank"}
+                  enterLeft={true}
+                />
+              </div>
+
+              <div className="w-full xl:w-5/12 text-3xl mt-20 space-y-3">
+                <h1 className="text-4xl font-semibold">Songtama Pig Farm</h1>
+                <p>0247583729</p>
+                <p>0200932904</p>
+                <p>Nyankpala, Near UDS Campus</p>
+              </div>
+            </section>
+
+            <section className="flex flex-wrap justify-center text-center mt-20">
+              <div className="w-full xl:w-4/12 text-base mt-20 space-y-3">
+                <div className="bg-slate-100 shadow-sm w rounded p-5 text-left text-emerald-500">
+                  <h1 className="text-2xl uppercase font-semibold">
+                    What we do
+                  </h1>
+                  <br />
+                  <p>
+                    Songtama Company LTD Specializes in various facets of pig
+                    farming. We spare no effort in producing high quality pigs
+                    for breeding, sales and meat.
+                  </p>
+                  <br />
+                  <p>
+                    We have spent years constantly refining our methods and
+                    processes to come up with the most effiecient way to get
+                    from <strong>Zero to Master Pig farmer.</strong> Aside from
+                    our produce we offer these lessons to interested parties.
+                  </p>
+                  <br />
+                  <br />
+                  <div className="text-center">
+                    <p>Buy From Us. Learn From Us</p>
+                    <p>Or Do Both!</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full xl:w-7/12">
+                <ImageWrapper
+                  src={"/svg/farm-girl.svg"}
+                  alt={"savings with a piggy bank"}
+                  enterLeft={false}
+                />
+              </div>
+            </section>
+
+            {/* <section className="flex flex-col w-full items-center mt-10">
+              <div className="w-full lg:w-3/4 h-72 bg-slate-200 rounded shadow text-center text-3xl ">
+                Video
+              </div>
+            </section> */}
+          </div>
+        </main>
+
+        <div className="flex flex-row h-16 w-full bg-slate-200 mt-20"></div>
+      </div>
+    </>
   );
 };
 
